98197d292bbcd3b2754afd220ed50904464d334b
Kamil <kamil.danielski@gmail.com>
2018-03-17 19:44:21 +0100

    Empty build - created from spring boot initliaizr

---
+++ src/main/java/eu/kamildanielski/manageyourlife/ManageYourLifeApplication.java
@@ -0,0 +1,12 @@
+package eu.kamildanielski.manageyourlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageYourLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageYourLifeApplication.class, args);
+	}
+}
