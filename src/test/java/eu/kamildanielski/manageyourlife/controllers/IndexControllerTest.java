e7f2f24649e1cedace4305d357823c200ca19094
Kamil <kamil.danielski@gmail.com>
2018-04-02 01:13:53 +0200

    Unit tests for IndexController and UserService

--- src/test/java/eu/kamildanielski/manageyourlife/controllers/IndexControllerTest.java
+++ src/test/java/eu/kamildanielski/manageyourlife/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package eu.kamildanielski.manageyourlife.controllers;
 
+import eu.kamildanielski.manageyourlife.domain.Expenditure;
 import eu.kamildanielski.manageyourlife.services.ExpenditureService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -30,12 +35,27 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+        //given
+        Set<Expenditure> expenditures = new HashSet<>();
+        Expenditure ex1 = new Expenditure();
+        ex1.setId(1L);
+        Expenditure ex2 = new Expenditure();
+        ex2.setId(2L);
+        expenditures.add((ex1));
+        expenditures.add((ex2));
 
+        ArgumentCaptor<Set<Expenditure>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        when(expenditureService.getExpenditures()).thenReturn(expenditures);
+
+        //when
         String viewName = controller.getIndexPage(model);
 
+        //then
         assertEquals("index", viewName);
         verify(expenditureService, times(1)).getExpenditures();
-        verify(model, times(1)).addAttribute(eq("expenditures"), anySet());
-
+        verify(model, times(1)).addAttribute(eq("expenditures"), argumentCaptor.capture());
+        Set<Expenditure> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
