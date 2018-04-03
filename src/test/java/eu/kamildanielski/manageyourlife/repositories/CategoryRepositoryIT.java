ed87e595a640ce08123d18e6d1885a973c9b52ae
Kamil <kamil.danielski@gmail.com>
2018-04-03 20:06:51 +0200

    Add Integration test for CategoryRepository

---
+++ src/test/java/eu/kamildanielski/manageyourlife/repositories/CategoryRepositoryIT.java
@@ -0,0 +1,39 @@
+package eu.kamildanielski.manageyourlife.repositories;
+
+import eu.kamildanielski.manageyourlife.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CategoryRepositoryIT {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByName() {
+        Optional<Category> categoryOptional = categoryRepository.findByName("Food");
+
+        assertEquals("Food", categoryOptional.get().getName());
+    }
+
+    @Test
+    public void findByName2() {
+        Optional<Category> categoryOptional = categoryRepository.findByName("Food");
+
+        assertEquals("All kinds of food", categoryOptional.get().getDescription());
+    }
+}
